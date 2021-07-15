579f214ef4e8772aca25efaf0b42882969684a52
DAngelo Ackerson <dackerson901@gmail.com>
2021-07-14 19:19:29 -0500

    updated js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var name = window.prompt("What is your name?");
 var amount = window.prompt("How many dogs do you have?")
 document.write("Hello " + name + ", Welcome to 901 Puppies! Do you like having " + amount + " dogs?");
