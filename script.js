4673822e3f1a5c2b19a1c55f42ab71d3cb90ca60
DAngelo Ackerson <dackerson901@gmail.com>
2021-07-14 19:44:32 -0500

    updated js

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ alert("Welcome to 901 Puppies!")
 var name = window.prompt("What is your name?");
 var amount = window.prompt("How many dogs do you have?")
 document.write("Hello " + name + ", Welcome to 901 Puppies! Do you like having " + amount + " dogs?");
+
+var isDog = true;
+var isTall = true;
+
+if(isDoglover && isYoung) {
+    document.write("You are young dog lover");
+} else {
+    document.write("You are either not young or not a doglover or both");
+}
